cca0b13deac2ccf91b19534983c68969b790bfba
Robin Gilks <g8ecj@gilks.org>
2012-06-22 13:15:54 +1200

    Moving on with implementing a terminal emulator

--- bertos/drv/term.h
+++ bertos/drv/term.h
@@ -1,8 +1,52 @@
+/**
+ * \file
+ * <!--
+ * This file is part of BeRTOS.
+ *
+ * Bertos is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, you may use this file as part of a free software
+ * library without restriction.  Specifically, if other files instantiate
+ * templates or use macros or inline functions from this file, or you compile
+ * this file and link it with other files to produce an executable, this
+ * file does not by itself cause the resulting executable to be covered by
+ * the GNU General Public License.  This exception does not however
+ * invalidate any other reasons why the executable file might be covered by
+ * the GNU General Public License.
+ *
+ * Copyright 2012 Robin Gilks (g8ecj at gilks.org)
+ *
+ * -->
+ *
+ * \brief Terminal emulator driver.
+ *
+ * Control codes and cursor addressing based on the old Newbury Data Recording 8000
+ * series dumb terminal which is nice and simple.
+ *
+ * Cursor positioning is done with [0x16][0x20+row][0x20+col]. All other codes are single control characters.
+ * The application generates a stream with the following control codes, the terminal emulator layer interpretss
+ * the codes for a specific device -
+ * eg. ANSI sequencies to send to a serial terminal,
+ *     direct cursor addressing on an LCD panel
+ *
+ * \author Robin Gilks <g8ecj@gilks.org>
+ *
+ */
 
-// cursor addressing based on the old Newbury Data Recording 9000 series dumb terminal
-
-// Cursor positioning is done with [0x16][0x20+row][0x20+col]
-// All other codes are single control characters
+#include "hw/thermo_map.h"
+#include "hw/hw_thermo.h"
 
 
 #define TERM_CPC     0x16     /**< Cursor position prefix - followed by row + column */
