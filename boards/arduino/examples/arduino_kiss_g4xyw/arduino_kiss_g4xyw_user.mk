d403aeb7b6c357cf85b0323f0fa8305d7f22e87c
Robin Gilks <g8ecj@gilks.org>
2013-02-24 16:27:38 +1300

    Add in the g4xyw modem KISS driver as an example project for Arduino

---
+++ boards/arduino/examples/arduino_kiss_g4xyw/arduino_kiss_g4xyw_user.mk
@@ -0,0 +1,43 @@
+#
+# User makefile.
+# Edit this file to change compiler options and related stuff.
+#
+
+# Programmer interface configuration, see http://dev.bertos.org/wiki/ProgrammerInterface for help
+arduino_kiss_g4xyw_PROGRAMMER_TYPE = none
+arduino_kiss_g4xyw_PROGRAMMER_PORT = none
+
+# Files included by the user.
+arduino_kiss_g4xyw_USER_CSRC = \
+	$(arduino_kiss_g4xyw_SRC_PATH)/main.c \
+	#
+
+# Files included by the user.
+arduino_kiss_g4xyw_USER_PCSRC = \
+	#
+
+# Files included by the user.
+arduino_kiss_g4xyw_USER_CPPASRC = \
+	#
+
+# Files included by the user.
+arduino_kiss_g4xyw_USER_CXXSRC = \
+	#
+
+# Files included by the user.
+arduino_kiss_g4xyw_USER_ASRC = \
+	#
+
+# Flags included by the user.
+arduino_kiss_g4xyw_USER_LDFLAGS = \
+	#
+
+# Flags included by the user.
+arduino_kiss_g4xyw_USER_CPPAFLAGS = \
+	#
+
+# Flags included by the user.
+arduino_kiss_g4xyw_USER_CPPFLAGS = \
+	-fno-strict-aliasing \
+	-fwrapv \
+	#
